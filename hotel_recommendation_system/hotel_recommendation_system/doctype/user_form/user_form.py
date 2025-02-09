8ea7376e84d6a8e01854704ad447dbd36f58e41c
akBear23 <ankhanhng0304@gmail.com>
2025-02-10 00:10:45 +0700

    delete unneccessary doctype

--- hotel_recommendation_system/hotel_recommendation_system/doctype/user_form/user_form.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, ankhanh and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class UserForm(Document):
-	pass
